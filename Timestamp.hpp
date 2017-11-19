352dd04ba7cc56c5943b680aee0c1584ef21d013
Konstantinos Kazatzis <kon.kazatzis@gmail.com>
2017-11-19 10:35:15 +0200

    Initial commit

---
+++ Timestamp.hpp
@@ -0,0 +1,49 @@
+#ifndef __Timestamp_hpp__
+#define __Timestamp_hpp__
+
+#include <iostream>
+#include <chrono>
+#include <string>
+
+namespace debug_tools{
+
+    /** A timestamp struct */
+    typedef struct Timestamp{
+        size_t year;
+        size_t month;
+        size_t day;
+        size_t hour;
+        size_t minutes;
+        size_t sec;
+        size_t msec;
+        Timestamp(int y, int m, int d, int h, int min, int s, int ms): 
+            year(y), month(m), day(d), hour(h), minutes(min), sec(s), msec(ms){};
+
+    } Timestamp_t;
+
+    /**
+      Get a string timestamp with hours, minutes, seconds and milliseconds
+      @return A timestamp in a string
+     */
+    std::string GetStringTimestamp();
+
+    /**
+      Get a current timestamp
+      @return A timestamp struct
+     */
+    Timestamp_t GetFullTimestamp();
+
+    /**
+        Get the amount of seconds passed since epoch
+        @return Seconds
+    */
+    uint64_t GetSecondsSinceEpoch();
+
+    /**
+        Get the amount of milliseconds passed since epoch
+        @return Milliseconds
+    */
+    uint64_t GetMSecondsSinceEpoch();
+}
+
+#endif
---
+++ test.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <assert.h>
+#include <vector>
+
+#include "FileLog.hpp"
+#include "Console.hpp"
+#include "Timestamp.hpp"
+#include "Timer.hpp"
+
+using namespace debug_tools;
+
+void function(){
+
+    FileLog & log = FileLog::GetInstance("whatever.txt");
+    log.Log(WARNING, "That a warning dude");
+
+}
+
+int main(int argc, char ** argv){
+    /* Testing timer */
+    Timer timer;
+    timer.Start();
+
+    /* Testing console debugging */
+    Console(INFO, "Hello!");
+    Console(WARNING, "That's a warning!");
+    Console(CRITICAL, "Tread lightly...");
+    Console(FATAL, "That's it! I am out.", DARK_BLUE);
+
+    std::cout << "---------------" << std::endl;
+
+    /* Testing custom text print */
+    CustomPrint(std::cout, "custom print1\n");
+    CustomPrint(std::cout, "custom print2\n", DARK_YELLOW);
+    CustomPrintHex(std::cerr, 254);
+    CustomPrintHex(std::cerr, -253, DARK_PURPLE);
+    CustomPrintHex(std::cout, 1.3);
+    CustomPrintHex(std::cout, -200.3, BLUE);
+
+	CustomInfo("i", 5);
+	CustomInfo("i", 5, "j", 6);
+	CustomInfoL("Loop arguments out of bounds", CRITICAL, "i", -1, "j", -4);
+
+    std::cout << "---------------" << std::endl;
+
+    /* Testing log file */
+    FileLog & log = FileLog::GetInstance("log.txt");
+    log.Log(INFO, "This is a piece of information!");
+
+    function();
+
+    /* Testing timestamp */
+    Timestamp_t date = GetFullTimestamp();
+    CustomInfoL("Timestamp", INFO,
+        "Year", date.year,
+        "Month", date.month,
+        "Day", date.day,
+        "Hour", date.hour,
+        "Minutes", date.minutes,
+        "Sec", date.sec,
+        "Msec", date.msec);
+
+    std::cout << "---------------" << std::endl;
+    
+    timer.Stop();
+    Console(INFO, "Time: " + timer.ToString() + " ms");
+
+    Console(INFO, "Epoch time: " + std::to_string(GetSecondsSinceEpoch()) + " s");
+    Console(INFO, "Epoch time: " + std::to_string(GetMSecondsSinceEpoch()) + " ms");
+
+    std::cout << "---------------" << std::endl;
+
+    /* Testing vector print */
+    std::vector<double> test = {1.0 ,1.2, 1.3, 1.4};
+    Print1dVector(test);
+    std::vector<std::string> test2 = {"a", "b", "c"};
+    Print1dVector(test, false);
+   
+    log.Log(FATAL, "Fatal error. exiting..."); 
+
+#ifdef _WIN32
+	system("pause");
+#endif
+
+    return 0;
+}
